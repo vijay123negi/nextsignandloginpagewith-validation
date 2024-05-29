b9a7302c108ddcb28449f8553019076635cee9c5
vijay123negi <negivijayvicky@gmail.com>
2024-05-29 12:17:13 +0530

    Add Initial Files

--- src/app/home/help/page.tsx
+++ src/app/home/help/page.tsx
@@ -1,12 +1,15 @@
 "use client"
+import Authlayout from '@/app/authlayout/page';
 import React from 'react';
 
 const Help = () => {
     return (
+        <Authlayout>
         <div>
             <h1>Help Page</h1>
             <p>this is the help page when the user is stucked in between can fllow the steps</p>
         </div>
+        </Authlayout>
     )
 }
 export default Help;
