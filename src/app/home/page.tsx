21fbe573918d04e76f3ce480a8d390d570092374
vijay123negi <negivijayvicky@gmail.com>
2024-06-03 11:01:19 +0530

    Add Initial Files

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -125,12 +125,14 @@ export default function PersistentDrawerRight() {
 
   const handleUserAdded = (newUser: User) => {
     setUsers([...users, newUser]);
+    setOpen(false);
     //setShowForm(false);
   };
   const handleUserUpdated = (updatedUser: User) => {
     setUsers(
       users.map((user) => (user.id === updatedUser.id ? updatedUser : user))
     );
+    setOpen(false);
     //setShowForm(false);
   };
 
